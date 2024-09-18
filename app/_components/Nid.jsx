40d348b7a26042bdaa7a7c55ff746779ecef9e74
sakib-xrz <sakibxrz21@gmail.com>
2024-09-19 01:27:57 +0600

    Remove unused page component and add Nid component for National ID card display

---
+++ app/_components/Nid.jsx
@@ -0,0 +1,158 @@
+"use client";
+
+import govt_logo from "@/public/govt_logo.png";
+import nid_bg from "@/public/nid_bg.png";
+import Image from "next/image";
+import user from "@/public/user.jpeg";
+
+import provider_sign from "@/public/provider_sign.png";
+import Barcode from "react-barcode";
+
+export default function Nid({ data }) {
+  const {
+    b_name,
+    e_name,
+    father_name,
+    mother_name,
+    dob,
+    nid_no,
+    signature,
+    house_or_holding,
+    village_or_rode,
+    post_office,
+    upazila,
+    zila,
+    blood_group,
+    birth_place,
+    delivery_date,
+  } = data;
+
+  return (
+    <div className="pointer-events-none flex select-none items-center justify-center gap-10 max-md:flex-col">
+      {/* front part */}
+
+      <div className="h-[204px] w-[324px] border-2 border-black font-medium">
+        <div className="flex h-[63px] items-center justify-center gap-1 border-b-2 border-black">
+          <Image src={govt_logo} alt="govt_logo" className="size-9" />
+          <div className="text-center">
+            {" "}
+            <h4 className="font-[family-name:var(--font-bangla)] text-sm font-[550]">
+              গণপ্রজাতন্ত্রী বাংলাদেশ সরকার
+            </h4>
+            <p className="font-[family-name:var(--font-english)] text-xs tracking-tight text-[#007700]">{`Government of the People's Republic of Bangladesh`}</p>
+            <p className="text-xs">
+              <span className="font-[family-name:var(--font-english)] text-[#FF0000]">
+                National ID Card
+              </span>{" "}
+              /{" "}
+              <span className="font-[family-name:var(--font-bangla)]">
+                জাতীয় পরিচয় পত্র
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <div className="relative -z-10 p-1">
+          <Image
+            src={nid_bg}
+            alt="nid_bg"
+            className="absolute left-1/2 right-0 top-1 size-28 -translate-x-1/2 transform opacity-70"
+          />
+
+          <div className="flex gap-2">
+            <div className="w-[70px]">
+              <Image
+                src={user}
+                alt="user"
+                className="h-[79px] w-[69px] border object-cover"
+              />
+              <div>
+                <p className="mt-1 text-center font-[family-name:var(--font-signature)] text-sm font-semibold">
+                  {signature}
+                </p>
+              </div>
+            </div>
+
+            <div className="space-y-.5 z-50">
+              <p className="text-sm">
+                নাম: <span className="pl-6 font-bold">{b_name}</span>
+              </p>
+              <p className="text-sm font-normal">
+                Name:
+                <span className="pl-[14px] font-[450] uppercase">{e_name}</span>
+              </p>
+              <p className="text-sm">
+                পিতা:
+                <span className="pl-[22px]">{father_name}</span>
+              </p>
+              <p className="text-sm">
+                মাতা:<span className="pl-[23px]">{mother_name}</span>
+              </p>
+              <p className="text-sm font-normal">
+                Date of Birth:{" "}
+                <span className="font-medium text-[#FF0000]">{dob}</span>
+              </p>
+              <p className="text-sm font-normal">
+                ID NO:{" "}
+                <span className="font-bold text-[#FF0000]">{nid_no}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* back part */}
+      <div className="h-[204px] w-[324px] border-2 border-black font-medium">
+        <div className="flex h-[36px] items-center justify-center gap-1 border-b-2 border-black">
+          <p className="px-2 py-1 font-[family-name:var(--font-bangla)] text-[8px] font-[450]">
+            এই কার্ডটি গণপ্রজাতন্ত্রী বাংলাদেশ সরকারের সম্পত্তি। কার্ডটি
+            ব্যবহারকারী ব্যতীত অন্য কোথাও পাওয়া গেলে নিকটস্থ পোস্ট অফিসে জমা
+            দেওয়ার জন্য অনুরোধ করা হলো
+          </p>
+        </div>
+
+        <div className="relative h-[68px] border-b-2 border-black">
+          <div>
+            <p className="px-2 py-1 text-center font-[family-name:var(--font-bangla)] text-[8.9px]">
+              ঠিকানা: বাসা/হোল্ডিং: {house_or_holding}, গ্রাম/রাস্তা:{" "}
+              {village_or_rode}, ডাকঘর: {post_office}, {upazila}, {zila}
+            </p>
+          </div>
+
+          <p className="absolute bottom-0.5 left-2 text-[8.9px]">
+            রক্তের গ্রুপ / Blood Group:{" "}
+            <span className="text-[10px] text-[#FF0000]">
+              {blood_group || ""}
+            </span>
+          </p>
+
+          <p className="absolute bottom-0.5 left-36 text-[8.9px]">
+            জন্মস্থান: {birth_place || ""}
+          </p>
+
+          {/* mudron no  */}
+          <p className="absolute bottom-0 right-0 bg-black text-[8.9px] text-white">
+            মূদ্রণ : ০১
+          </p>
+        </div>
+
+        <div className="p-2">
+          <div className="flex items-end justify-between">
+            <Image src={provider_sign} alt="provider_sign" className="w-28" />
+            <p className="pb-0.5 font-[family-name:var(--font-bangla)] text-[8.9px] font-[450]">
+              প্রদানের তারিখ: ২৯/১১/২০২১
+            </p>
+          </div>
+          <div>
+            <Barcode
+              className="h-fit w-[305px]"
+              displayValue={false}
+              height={85}
+              value={`${e_name}, ${dob}`}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,145 +1,133 @@
-import Container from "@/components/shared/Container";
-import { Button } from "antd";
-import govt_logo from "@/public/govt_logo.png";
-import nid_bg from "@/public/nid_bg.png";
-import Image from "next/image";
-import user from "@/public/user.jpeg";
+"use client";
 
-import provider_sign from "@/public/provider_sign.png";
+import Container from "@/components/shared/Container";
+import Nid from "./_components/Nid";
+import { Card, DatePicker, Input } from "antd";
+import Label from "@/components/shared/Label";
+import { useFormik } from "formik";
+import FormikError from "@/components/shared/FormikError";
+import moment from "moment";
+
+const data = {
+  b_name: "মোঃ সাকীবুল ইসলাম",
+  e_name: "MD SAKIBUL ISLAM",
+  father_name: "মোঃ আব্দুস সালাম সিকদার",
+  mother_name: "মোসাঃ শাহীনা বেগম",
+  // dob: "22 Aug 1999",
+  dob: "",
+  nid_no: "4207590631",
+  signature: "Sakibul Islam",
+  house_or_holding: "১৭",
+  village_or_rode: "ইস্টার্ন হাউজিং, ব্লক-জে,রোড-এন/৮, দ্বিগুন",
+  post_office: "মিরপুর - ৮৭৩০",
+  upazila: "ঢাকা উত্তর সিটি  কর্পোরেশন",
+  zila: "ঢাকা",
+  blood_group: "B+",
+  birth_place: "বরগুনা",
+  delivery_date: "২৯/১১/২০২১",
+};
 
 export default function Home() {
+  const formik = useFormik({
+    initialValues: data,
+    // validationSchema: {},
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  console.log(formik.values.dob);
+
   return (
     <Container>
-      <Button type="primary">Hello World</Button>
-
-      <br />
-      <br />
-      <br />
-
-      <div className="flex items-center justify-center gap-10">
-        {/* front part */}
-
-        <div className="h-[204px] w-[324px] border-2 border-black font-medium">
-          <div className="flex h-[63px] items-center justify-center gap-1 border-b-2 border-black">
-            <Image src={govt_logo} alt="govt_logo" className="size-9" />
-            <div className="text-center">
-              {" "}
-              <h4 className="font-[family-name:var(--font-bangla)] text-sm font-[550]">
-                গণপ্রজাতন্ত্রী বাংলাদেশ সরকার
-              </h4>
-              <p className="font-[family-name:var(--font-english)] text-xs tracking-tight text-[#007700]">{`Government of the People's Republic of Bangladesh`}</p>
-              <p className="text-xs">
-                <span className="font-[family-name:var(--font-english)] text-[#FF0000]">
-                  National ID Card
-                </span>{" "}
-                /{" "}
-                <span className="font-[family-name:var(--font-bangla)]">
-                  জাতীয় পরিচয় পত্র
-                </span>
-              </p>
-            </div>
-          </div>
-
-          <div className="relative -z-10 p-1">
-            <Image
-              src={nid_bg}
-              alt="nid_bg"
-              className="absolute left-1/2 right-0 top-1 size-28 -translate-x-1/2 transform opacity-70"
-            />
-
-            <div className="flex gap-2">
-              <div className="w-[70px]">
-                <Image
-                  src={user}
-                  alt="user"
-                  className="h-[79px] w-[69px] border object-cover"
+      {/* <Nid data={data} /> */}
+      <div className="mx-auto sm:w-8/12 md:w-6/12">
+        <Card>
+          <h4 className="text-center font-[family-name:var(--font-english)] font-semibold text-primary max-sm:text-2xl sm:text-3xl">
+            Old NID Card Maker
+          </h4>
+          <br />
+          <hr />
+          <br />
+          <form>
+            <div className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="b_name" required>
+                  Name (বাংলায়)
+                </Label>
+                <Input
+                  id="b_name"
+                  name="b_name"
+                  placeholder="আপনার নাম লিখুন"
+                  {...formik.getFieldProps("b_name")}
                 />
-                <div>
-                  <p className="mt-1 text-center font-[family-name:var(--font-signature)] text-sm font-semibold">
-                    Md Sakibul Islam
-                  </p>
-                </div>
+                <FormikError formik={formik} name="b_name" />
               </div>
-
-              <div className="space-y-.5 z-50">
-                <p className="text-sm">
-                  নাম: <span className="pl-6 font-bold">মোঃ সাকীবুল ইসলাম</span>
-                </p>
-                <p className="text-sm font-normal">
-                  Name:
-                  <span className="pl-[14px] font-[450] uppercase">
-                    MD SAKIBUL ISLAM
-                  </span>
-                </p>
-                <p className="text-sm">
-                  পিতা:
-                  <span className="pl-[22px]">মোঃ আব্দুস সালাম সিকদার</span>
-                </p>
-                <p className="text-sm">
-                  মাতা:<span className="pl-[23px]">মোসাঃ শাহীনা বেগম</span>
-                </p>
-                <p className="text-sm font-normal">
-                  Date of Birth:{" "}
-                  <span className="font-medium text-[#FF0000]">
-                    {" "}
-                    22 Aug 1999
-                  </span>
-                </p>
-                <p className="text-sm font-normal">
-                  ID NO:{" "}
-                  <span className="font-bold text-[#FF0000]">4207590631</span>
-                </p>
+              <div className="space-y-1">
+                <Label htmlFor="e_name" required>
+                  Name (in English)
+                </Label>
+                <Input
+                  id="e_name"
+                  name="e_name"
+                  placeholder="Enter your name"
+                  {...formik.getFieldProps("e_name")}
+                />
+                <FormikError formik={formik} name="e_name" />
+              </div>
+              <div className="space-y-1">
+                <Label
+                  htmlFor="father_name"
+                  required
+                >{`Father's Name (বাংলায়)`}</Label>
+                <Input
+                  id="father_name"
+                  name="father_name"
+                  placeholder="আপনার পিতার নাম লিখুন"
+                  {...formik.getFieldProps("father_name")}
+                />
+                <FormikError formik={formik} name="father_name" />
+              </div>
+              <div className="space-y-1">
+                <Label
+                  htmlFor="mother_name"
+                  required
+                >{`Mother's Name (বাংলায়)`}</Label>
+                <Input
+                  id="mother_name"
+                  name="mother_name"
+                  placeholder="আপনার মাতার নাম লিখুন"
+                  {...formik.getFieldProps("mother_name")}
+                />
+                <FormikError formik={formik} name="mother_name" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="signature" required>
+                  Signature (in English)
+                </Label>
+                <Input
+                  id="signature"
+                  name="signature"
+                  placeholder="Enter your signature"
+                  {...formik.getFieldProps("signature")}
+                />
+                <FormikError formik={formik} name="signature" />
+              </div>
+              <div className="flex w-full flex-col gap-1">
+                <Label htmlFor="dob" required>
+                  Date of Birth
+                </Label>
+                <DatePicker
+                  placeholder="Select your date of birth"
+                  onChange={(_, dateString) => {
+                    formik.setFieldValue("dob", dateString);
+                  }}
+                />
               </div>
             </div>
-          </div>
-        </div>
-
-        {/* back part */}
-        <div className="h-[204px] w-[324px] border-2 border-black font-medium">
-          <div className="flex h-[36px] items-center justify-center gap-1 border-b-2 border-black">
-            <p className="px-2 py-1 font-[family-name:var(--font-bangla)] text-[8px] font-[450]">
-              এই কার্ডটি গণপ্রজাতন্ত্রী বাংলাদেশ সরকারের সম্পত্তি। কার্ডটি
-              ব্যবহারকারী ব্যতীত অন্য কোথাও পাওয়া গেলে নিকটস্থ পোস্ট অফিসে জমা
-              দেওয়ার জন্য অনুরোধ করা হলো
-            </p>
-          </div>
-
-          <div className="relative h-[68px] border-b-2 border-black">
-            <div>
-              <p className="px-2 py-1 text-center font-[family-name:var(--font-bangla)] text-[8.9px]">
-                ঠিকানা: বাসা/হোল্ডিং: ১৭, গ্রাম/রাস্তা: ইস্টার্ন হাউজিং,
-                ব্লক-জে, রোড-এন/৮, দ্বিগুন, ডাকঘর: মিরপুর - ৮৭৩০, পল্লবী, ঢাকা
-                উত্তর সিটি কর্পোরেশন, ঢাকা
-              </p>
-            </div>
-
-            <p className="absolute bottom-0.5 left-2 text-[8.9px]">
-              রক্তের গ্রুপ / Blood Group:{" "}
-              <span className="text-[10px] text-[#FF0000]">O+</span>
-            </p>
-
-            <p className="absolute bottom-0.5 left-36 text-[8.9px]">
-              জন্মস্থান: বরগুনা
-            </p>
-
-            {/* mudron no  */}
-            <p className="absolute bottom-0 right-0 bg-black text-[8.9px] text-white">
-              মূদ্রণ : ০১
-            </p>
-          </div>
-
-          <div className="p-2">
-            <div className="flex items-end justify-between">
-              <Image src={provider_sign} alt="provider_sign" className="w-28" />
-              <p className="pb-0.5 font-[family-name:var(--font-bangla)] text-[8.9px] font-[450]">
-                প্রদানের তারিখ: ২৯/১১/২০২১
-              </p>
-            </div>
-          </div>
-        </div>
+          </form>
+        </Card>
       </div>
     </Container>
   );
 }
-
-// 134 * 152
